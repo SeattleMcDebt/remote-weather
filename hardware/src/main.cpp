e8093baaa4067cc23c6dc9b99970bfc8bdc842f9
Alex Stout <stoutab4384@uwec.edu>
2020-10-29 13:01:32 -0500

    add platform.io testing

--- hardware/src/main.cpp
+++ hardware/src/main.cpp
@@ -1,9 +1,26 @@
-#include <Arduino.h>
+#include "Arduino.h"
 
-void setup() {
-  // put your setup code here, to run once:
+#ifndef LED_BUILTIN
+#define LED_BUILTIN 13
+#endif
+
+void setup()
+{
+  // initialize LED digital pin as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_BUILTIN, HIGH);
+
+  // wait for a second
+  delay(1000);
+
+  // turn the LED off by making the voltage LOW
+  digitalWrite(LED_BUILTIN, LOW);
+
+   // wait for a second
+  delay(1000);
 }
